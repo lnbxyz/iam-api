174027ff4a67c446840f8adb86b94802639c338d
lunaboona <hyaluna@gmail.com>
2022-03-15 21:19:53 -0300

    add route

--- server.ts
+++ server.ts
@@ -1,10 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({ message: 'hewwo world' });
+});
 
 try {
   app.listen(port, (): void => {
